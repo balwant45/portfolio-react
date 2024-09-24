b828cb63a246d2461b2d3c502f488ad155a9114b
balwant <bawabalwant045@gmail.com>
2024-09-24 23:03:28 +0530

    create myskill,navbar,landing & social media component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { useEffect, useState } from "react";
 import useMediaQuery from './hooks/useMediaQuery.jsx'
 import Navbar from "./scenes/Navbar.jsx";
 import DotGroup from './scenes/DotGroup';
+import Landing from "./scenes/Landing.jsx";
+import LineGradient from "./component/LineGradient.jsx";
+import MySkills from "./scenes/MySkills.jsx";
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
+  const [selectedPage, setSelectedPage] = useState("home");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens=useMediaQuery("(min-width:1060px)");
 
   useEffect(()=>{
     const handleScroll=()=>{
-      if(window.scrollY===0) setIsTopOfPage(true);
+      if(window.scrollY===0) {setIsTopOfPage(true); setSelectedPage("home");};
       if(window.scrollY !==0) setIsTopOfPage(false);
     }
   window.addEventListener('scroll',handleScroll);
@@ -21,17 +24,22 @@ function App() {
   return (
   <div className="app bg-deep-blue">
     <Navbar
+     isTopOfPage={isTopOfPage}
       selectdPage={selectedPage}
       setSelectdPage={setSelectedPage}
     />
     <div className="w-5/6 mx-auto md:h-full">
-    {isAboveMediumScreens && (<DotGroup
+    {isAboveMediumScreens && (
+      <DotGroup
      selectedPage={selectedPage}
      setSelectedPage={setSelectedPage}
-
     />)}
-
+    <Landing/>
     </div>
+<LineGradient/>
+<div className="w-5/6 mx-auto md:h-full">
+  <MySkills/>
+</div>
   </div>)
 }
 
