76b3022d717490decde58d25a0c946b8bf82fac8
balwant <bawabalwant045@gmail.com>
2025-06-24 02:38:55 +0530

    before final commit

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -30,75 +30,79 @@ function Landing({ setSelectedPage }) {
             src="./assets/profile-image.png"
           />
         )}
-        </div>
+      </div>
       {/* text content */}
-        <div className="z-30 basis-2/5 mt-12 md:mt-32 ">
+      <div className="z-30 basis-2/5 mt-12 md:mt-32 ">
         {/* heading */}
         <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{once:true, amount:0.5 }}
-        transition={{duration:0.5}}
-        variants={{hidden:{opacity:0, x:-50},visible:{opacity:1, x:0}
-        }}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
         >
-        <p className="text-6xl font-playfair z-10 text-center md:text-start">
-          Balwant {""}
-          <span  className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Balwant {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
               before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
-           >
-          Singh
-          </span>
-        </p>
-        <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            >
+              Singh
+            </span>
+          </p>
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
             Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
             viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
           </p>
         </motion.div>
 
-{/* call to action */}
-        <motion.div 
-        className="flex mt-5 justify-center md:justify-start"
-         initial="hidden"
-        whileInView="visible"
-        viewport={{once:true, amount:0.5 }}
-        transition={{delay:0.2, duration:0.5}}
-        variants={{hidden:{opacity:0, x:-50},
-        visible:{opacity:1, x:0}
-        }}>
-        <AnchorLink className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue"
-        onClick={()=>setSelectedPage("contact")}
-        href="#contact"
+        {/* call to action */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
         >
-          Contact Me
-        </AnchorLink>
-        <AnchorLink
-        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
-        onClick={()=>setSelectedPage("contact")
-        }
-        href="#contact"
-        >
-<div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10">
-  Let's talk. 
-</div>
-        </AnchorLink>
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink
+            className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            <div className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex items-center justify-center font-playfair px-10">
+              Let's talk.
+            </div>
+          </AnchorLink>
         </motion.div>
-        
-        <motion.div 
-        className="flex mt-5 justify-center md:justify-start"
-         initial="hidden"
-        whileInView="visible"
-        viewport={{once:true, amount:0.5 }}
-        transition={{delay:0.4, duration:0.5}}
-        variants={{hidden:{opacity:0, x:-50},
-        visible:{opacity:1, x:0}
-        }}>
-<SocialMediaIcons/>
-        </motion.div>
-        </div>
 
-      
-     
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <SocialMediaIcons />
+        </motion.div>
+      </div>
     </section>
   );
 }
